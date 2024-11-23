78a38d5ed3efbe9d7b05e65519e586a4b2f358d3
Nekoth <alexandre.deletraz@hotmail.com>
2024-11-23 10:48:43 +0100

    temp commit

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -9,18 +9,18 @@ public class Main {
   public static void main(String[] args) {
     // Define command name - source: https://stackoverflow.com/a/11159435
     String jarFilename =
-        new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
-            .getName();
+            new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())
+                    .getName();
 
     // Create root command
     Root root = new Root();
 
     // Execute command and get exit code
     int exitCode =
-        new CommandLine(root)
-            .setCommandName(jarFilename)
-            .setCaseInsensitiveEnumValuesAllowed(true)
-            .execute(args);
+            new CommandLine(root)
+                    .setCommandName(jarFilename)
+                    .setCaseInsensitiveEnumValuesAllowed(true)
+                    .execute(args);
 
     System.exit(exitCode);
   }
--- src/main/java/ch/heigvd/dai/commands/Root.java
+++ src/main/java/ch/heigvd/dai/commands/Root.java
@@ -3,12 +3,12 @@ package ch.heigvd.dai.commands;
 import picocli.CommandLine;
 
 @CommandLine.Command(
-    description = "A small game to experiment with TCP.",
-    version = "1.0.0",
-    subcommands = {
-      Client.class,
-      Server.class,
-    },
-    scope = CommandLine.ScopeType.INHERIT,
-    mixinStandardHelpOptions = true)
+        description = "A small game to experiment with TCP.",
+        version = "1.0.0",
+        subcommands = {
+                Client.class,
+                Server.class,
+        },
+        scope = CommandLine.ScopeType.INHERIT,
+        mixinStandardHelpOptions = true)
 public class Root {}
